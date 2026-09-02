e2a43a4a665abcc42906336d5ee0321bc5f30ee5
hezruh <hezra@hawaii.edu>
2026-09-02 12:57:18 -1000

    Exercise 2

--- Lab 2/Ex1.py
+++ Lab 2/Ex1.py
@@ -1,3 +1,6 @@
+# Asks user for number between 1 and 100, squares it, and prints the result.
+# Name: Hezra Calventas
+# Date: 9/2/2026
 valueEntered = input("give me a whole number between 1 and 100: ")
 valueAsInt = int(valueEntered)
 
---
+++ Lab 2/Ex2.py
@@ -0,0 +1,7 @@
+# Ask user to enter the year they were born, and calculate their age based on the current year (2026).
+# Name: Hezra Calventas
+# Date: 9/2/2026
+
+yearOfBirth = int(input("Please enter the year that you were born: "))
+currentYear = 2026
+print(f"You were born in {yearOfBirth} and you are {currentYear - yearOfBirth} years old.")
